8d1a2ffbb3c1821ec908c2cdc16f4e83b12c905c
Dominik Vinš <domik.mbvins@seznam.cz>
2025-10-31 19:27:44 +0100

    ios fix snad?

--- lib/src/racebox_connection.dart
+++ lib/src/racebox_connection.dart
@@ -1,6 +1,6 @@
-
 import 'dart:async';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:io';
 
 /// Handles the connection to a single RaceBox device.
 class RaceBoxConnection {
@@ -8,11 +8,13 @@ class RaceBoxConnection {
 
   BluetoothCharacteristic? _txCharacteristic;
   StreamSubscription<List<int>>? _notificationSubscription;
-  StreamController<List<int>> _dataStreamController = StreamController.broadcast();
+  StreamController<List<int>> _dataStreamController =
+      StreamController.broadcast();
 
   /// The stream of data from the RaceBox device.
   Stream<List<int>> get dataStream => _dataStreamController.stream;
-  Stream<BluetoothConnectionState> get connectionState => device.connectionState;
+  Stream<BluetoothConnectionState> get connectionState =>
+      device.connectionState;
 
   BluetoothCharacteristic? get characteristic => _txCharacteristic;
 
@@ -22,18 +24,26 @@ class RaceBoxConnection {
   /// Returns true if successful, false otherwise.
   Future<bool> connect() async {
     try {
-      await device.connect(autoConnect: false, timeout: const Duration(seconds: 15));
-      await device.requestMtu(247);
-      await device.requestConnectionPriority(connectionPriorityRequest: ConnectionPriority.high);
+      await device.connect(
+          autoConnect: false, timeout: const Duration(seconds: 15));
+      if (Platform.isAndroid) {
+        await device.requestMtu(247);
+        await device.requestConnectionPriority(
+            connectionPriorityRequest: ConnectionPriority.high);
+      }
 
       List<BluetoothService> services = await device.discoverServices();
+      await Future.delayed(const Duration(milliseconds: 500));
       for (BluetoothService service in services) {
         if (service.uuid == Guid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")) {
-          for (BluetoothCharacteristic characteristic in service.characteristics) {
-            if (characteristic.uuid == Guid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")) {
+          for (BluetoothCharacteristic characteristic
+              in service.characteristics) {
+            if (characteristic.uuid ==
+                Guid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")) {
               _txCharacteristic = characteristic;
               await _txCharacteristic!.setNotifyValue(true);
-              _notificationSubscription = _txCharacteristic!.lastValueStream.listen((value) {
+              _notificationSubscription =
+                  _txCharacteristic!.lastValueStream.listen((value) {
                 _dataStreamController.add(value);
               });
               return true;
